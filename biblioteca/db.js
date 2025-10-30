8e98d3a9d9a5a82dcb4bc0ad93855e2694be6217
Lucas Casemiro <141570809+lucascasemiro@users.noreply.github.com>
2025-10-30 12:33:54 -0300

    Update db.js

--- biblioteca/db.js
+++ biblioteca/db.js
@@ -1,8 +1,16 @@
 const express =  require("express")
 const app = express();
+const path =  require("path")
 
-app.get('/', (req, res) =>{
-res.send('<h1>opa</h1>')
-})
-
+app.post('/pg', (req, res) =>{
+    const dados = req.body
+    res.send('<h1>pg</h1>')
+    })
+    app.put('/pg:id', (req, res) =>{
+        res.send(req.params.id)
+        res.send('<h1>att</h1>')
+        })
+        app.get('/', (req, res) =>{
+            res.sendFile(path.join(__dirname, 'index.html'))
+            })
 app.listen(4000)
